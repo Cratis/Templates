02d18d96b349e87dcc484243ec29d4e191be3abf
woksin <sindrewilting@gmail.com>
2026-02-23 14:03:39 +0100

    Update Chronicle Web App configuration and streamline initialization

--- Templates/ChronicleWeb/Program.cs
+++ Templates/ChronicleWeb/Program.cs
@@ -1,17 +1,8 @@
-using Cratis.Chronicle;
-using Cratis.Chronicle.Connections;
-
 Console.WriteLine("Starting Chronicle Web App...");
 var app = WebApplication.CreateBuilder(args)
-    .AddCratisChronicle(_ => _.ConnectionString = ChronicleConnectionString.Development)
+    .AddCratisChronicle()
     .Build();
 
 app.UseCratisChronicle();
 
-using var serviceScope = app.Services.CreateScope();
-var services = serviceScope.ServiceProvider;
-var eventStore = services.GetRequiredService<IEventStore>();
-Console.WriteLine("Appending event");
-await eventStore.EventLog.Append("some-event-source", new TestEvent("Hello world!"));
-
-app.Run();
+await app.RunAsync();
