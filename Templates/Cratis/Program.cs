81136e6766932395e3ead6f637fa6e63870107ec
woksin <sindrewilting@gmail.com>
2026-03-01 16:08:18 +0100

    Enhance Cratis template: add MongoDB package installation instructions, refactor registration feature, and update appsettings for route configuration

--- Templates/Cratis/Program.cs
+++ Templates/Cratis/Program.cs
@@ -1,35 +1,28 @@
-using AnApp.Features.SomeFeature.Registration;
+using Cratis.Arc.MongoDB;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.AddCratis();
-
+builder.AddCratis(
+    configureArcChronicleOptions: options => options.WithCamelCaseNamingPolicy(),
+    configureArcBuilder: arcBuilder => arcBuilder.WithMongoDB(configureMongoDB: builder => builder.WithCamelCaseNamingPolicy()));
+    
 builder.Services.AddControllers();
 builder.Services.AddMvc();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => options.AddConcepts());
 
 var app = builder.Build();
+
 app.UseRouting();
-app.UseCratis();
-
-app.MapControllers();
-app.UseWebSockets();
-
-app.UseSwagger();
-app.UseSwaggerUI();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
+
+app.UseWebSockets();
+app.MapControllers();
+app.UseCratis();
+ 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapFallbackToFile("/index.html");
 
 await app.RunAsync();
-
-
-public class RegistrationReactor(ILogger<RegistrationReactor> logger) : IReactor
-{
-    public Task Handle(Registered evt)
-    {
-        logger.LogInformation("Registered: {Name}", evt.Name);
-        return Task.CompletedTask;
-    }
-}
