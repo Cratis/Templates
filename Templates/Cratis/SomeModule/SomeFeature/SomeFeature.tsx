6b1eef1ba800359a5f99924bddf551d0ab88cdb7
Einar <einari@me.com>
2026-03-02 08:55:58 +0100

    Improving cohesion

--- Templates/Cratis/SomeModule/SomeFeature/SomeFeature.tsx
+++ Templates/Cratis/SomeModule/SomeFeature/SomeFeature.tsx
@@ -1,11 +1,8 @@
 import { useState } from 'react';
-import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
-import { DataTableForObservableQuery } from '@cratis/components/DataTables';
-import { CommandDialog } from '@cratis/components/CommandDialog';
-import { InputTextField } from '@cratis/components/CommandForm';
-import { AllListings } from 'Api/SomeFeature/Listing/AllListings';
-import { Register } from 'Api/SomeFeature/Registration/Register';
+
+import { Registration } from './Registration/Registration';
+import { ListingDataTable } from './Listing/ListingDataTable';
 
 export const SomeFeature = () => {
     const [showRegister, setShowRegister] = useState(false);
@@ -19,26 +16,8 @@ export const SomeFeature = () => {
                     icon='pi pi-plus'
                     onClick={() => setShowRegister(true)} />
             </div>
-            <DataTableForObservableQuery
-                query={AllListings}
-                dataKey='eventSourceId'
-                emptyMessage='No items registered yet.'>
-                <Column field='name' header='Name' />
-                <Column field='eventSourceId' header='Id' />
-            </DataTableForObservableQuery>
-            <CommandDialog
-                command={Register}
-                visible={showRegister}
-                header='Register'
-                confirmLabel='Register'
-                cancelLabel='Cancel'
-                onConfirm={result => { if (result.isSuccess) setShowRegister(false); }}
-                onCancel={() => setShowRegister(false)}>
-                <InputTextField<Register>
-                    value={c => c.name}
-                    title='Name'
-                    icon={<i className='pi pi-pencil' />} />
-            </CommandDialog>
+            <ListingDataTable/>
+            <Registration showRegister={showRegister} setShowRegister={setShowRegister} />
         </div>
     );
 };
