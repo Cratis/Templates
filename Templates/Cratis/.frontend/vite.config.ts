50202d13065c85f742ea21cb37a16bd0f88971a8
Einar Ingebrigtsen <einari@me.com>
2026-03-27 11:57:14 +0100

    Update proxy path in Vite configuration

--- Templates/Cratis/.frontend/vite.config.ts
+++ Templates/Cratis/.frontend/vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
         port: 9000,
         open: true,
         proxy: {
-            ".cratis/me": {
+            ".cratis": {
                 target: 'http://localhost:5000',
                 ws: true
             },
